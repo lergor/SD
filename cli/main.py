577f8b6c06138bdbcfb58c80ad3cbf6e74171808
lergor <gor.ler177@gmail.com>
2018-03-24 17:07:55 +0300

    Added the 'grep' command, tests for it and uml diagram.

--- cli/main.py
+++ cli/main.py
@@ -2,9 +2,6 @@
 
 from src.cli import Cli
 
-
 if __name__ == '__main__':
     cli = Cli()
     cli.run()
-    # cli.process_input('\x65\x63\x68\x6F\x20\x6B\x65\x6B')
-    # print('\x65\x63\x68\x6F\x20\x6B\x65\x6B')
--- cli/test/test_cli.py
+++ cli/test/test_cli.py
@@ -9,14 +9,14 @@ class testCli(unittest.TestCase):
 
     def test_commands(self):
         result = self.cli.process_input('echo \'Hello, World!\'')
-        self.assertEqual('Hello, World!' + os.linesep, result.get_output())
+        self.assertEqual('Hello, World!', result.get_output())
         result = self.cli.process_input('echo \'Hello, World!\' | cat')
         self.assertEqual('Hello, World!' + os.linesep, result.get_output())
 
         self.cli.process_input('FILE=test_text.txt')
         result = self.cli.process_input('cat $FILE test_env.py | wc')
         result = (result.get_output()).split()
-        right_result = ['30', '109', '905']
+        right_result = ['31', '110', '906']
         self.assertEqual(len(right_result), len(result))
         for i in range(len(result)):
             self.assertEqual(right_result[i], result[i])
